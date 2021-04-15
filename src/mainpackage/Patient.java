e7f99e219a8f11de13d16c7743a5636aeba866b6
MARK-I <nikostyliyis@gmail.com>
2021-04-15 12:28:00 +0300

    σδφγσφσαφ

--- src/mainpackage/Patient.java
+++ src/mainpackage/Patient.java
@@ -1,10 +1,31 @@
 package mainpackage;
 
 public class Patient extends Users{
-    int afm;
+    int amka;
     public Patient( String u,String p,String n ,String s,int af ) {
         super(u,p,n,s);
-        this.afm=af;
+        this.amka=af;
+
+    }
+
+    public void registration(){
+
+
+    }
+
+    public void search_rad_doc(){
+
+    }
+
+    public void search_rad_eid(){
+
+    }
+
+    public void show_rad(){
+
+    }
+
+    public void show_hist(){
 
     }
 }
